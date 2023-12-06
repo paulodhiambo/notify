9e429ef44256dfaba093363c09119110e750a5ab
Paul Odhiambo <paulodhiambo962@gmail.com>
2023-12-06 10:50:52 +0300

    adding model details

--- web/models/tenant_model.go
+++ web/models/tenant_model.go
@@ -1,5 +1,35 @@
-package main
+package models
 
-func main() {
+import "gorm.io/gorm"
 
+type SmsConfiguration struct {
+	ApiKey    string `json:"apiKey"`
+	ApiSecret string `json:"ApiSecret"`
+	Host      string `json:"host"`
+	gorm.Model
+}
+
+type EmailConfiguration struct {
+	Mail     string `json:"mail"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Password string `json:"password"`
+	Username string `json:"username"`
+	gorm.Model
+}
+
+type PushNotificationConfiguration struct {
+	ApiKey string `json:"apiKey"`
+	Host   string `json:"host"`
+	Secret string `json:"secret"`
+	gorm.Model
+}
+
+type SlackConfiguration struct {
+	gorm.Model
+}
+
+type Configuration struct {
+	PartyCode string `json:"partyCode"`
+	gorm.Model
 }
